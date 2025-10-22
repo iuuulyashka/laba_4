9b78c863d840a2cf09e49437f237516995b1ca9d
Egor Balyberdin <145469118+iuuulyashka@users.noreply.github.com>
2025-10-22 15:31:21 +0200

    Create UserInfo.php

---
+++ www/UserInfo.php
@@ -0,0 +1,28 @@
+<?php
+class UserInfo {
+    public static function getClientInfo(): array {
+        return [
+            'ip_address' => $_SERVER['REMOTE_ADDR'] ?? 'Неизвестно',
+            'user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? 'Неизвестно',
+            'server_time' => date('Y-m-d H:i:s'),
+            'request_method' => $_SERVER['REQUEST_METHOD'] ?? 'Неизвестно'
+        ];
+    }
+
+    public static function saveSubmissionTime(): void {
+        setcookie('last_submission', date('Y-m-d H:i:s'), time() + 3600, '/');
+    }
+
+    public static function getLastSubmission(): string {
+        return $_COOKIE['last_submission'] ?? 'Еще не было отправок';
+    }
+
+    // Метод строго по заданию
+    public static function getInfo(): array {
+        return [
+            'ip' => $_SERVER['REMOTE_ADDR'] ?? '',
+            'user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? '',
+            'time' => date('Y-m-d H:i:s')
+        ];
+    }
+}
